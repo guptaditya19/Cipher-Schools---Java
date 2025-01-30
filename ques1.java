93f68820548e81e9cf8953c5934a67d6ef69aea8
Aditya Gupta <150150951+guptaditya19@users.noreply.github.com>
2025-01-30 11:40:48 +0530

    Add files via upload

---
+++ ques1.java
@@ -0,0 +1,18 @@
+public class ques1 {
+    public static void main(String[] args) {
+        int intVar = 10;
+        double doubleVar = 20.5;
+        char charVar = 'A';
+        boolean boolVar = true;
+        System.out.println("Before conversion:");
+        System.out.println("intVar: " + intVar);
+        System.out.println("doubleVar: " + doubleVar);
+        System.out.println("charVar: " + (int)charVar);
+        System.out.println("boolVar: " + boolVar);
+        double intToDouble = intVar;
+        int charToInt = charVar; 
+        System.out.println("After conversion:");
+        System.out.println("intToDouble: " + intToDouble);
+        System.out.println("charToInt: " + charToInt);
+    }
+}
